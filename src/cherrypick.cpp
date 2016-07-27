b5aa499412c185833699eb26f1a002730d45c7ca
Jasmine Chen <j.lin013@gmail.com>
2016-07-27 21:28:54 +0800

    Forge the new massif file (massif-cherrypick core functionality complete)

--- src/cherrypick.cpp
+++ src/cherrypick.cpp
@@ -23,14 +23,34 @@ inline dtSz_t getSz(const char *s) {
 	int i;
 	dtSz_t ret = 0;
 	for (i = 0; s[i] != ':'; ++i) ;
-	for (i = 0; s[i] != ' '; ++i) ;
-	for ( ; (s[i] < 48 || s[i] > 57) && s[i]; ++i) ;
+	for ( ; s[i] != ' '; ++i) ;
+	for ( ; s[i] < 48 || s[i] > 57; ++i) ;
 	for ( ; s[i] >= 48 && s[i] <= 57; ++i)
 		ret = ret * 10 + s[i] - 48;
 //	printf(s);
 //	printf("%llu\n", ret);
 	return ret;
 }
+
+inline void printSz(FILE *fo, const char *s, dtSz_t sz_new) {
+	int i;
+	char dgts[50];
+	if (sz_new > 0) {
+		for (i = 0; sz_new; sz_new /= 10)
+			dgts[i++] = sz_new % 10;
+	} else {
+		dgts[0] = 0;
+		i = 1;
+	}
+	int top = i - 1;
+	for (i = 0; s[i] != ':'; ++i) fputc(s[i], fo);
+	for ( ; s[i] != ' '; ++i) fputc(s[i], fo);
+	int pos = i + 1;
+	for (i = top; i >= 0; --i) fputc(dgts[i] + 48, fo);
+	for (i = pos; s[i] >= 48 && s[i] <= 57; ++i) ;
+	for ( ; s[i]; ++i) fputc(s[i], fo);
+}
+
 
 const int MAX_LEN  = 1002;
 
@@ -48,8 +68,7 @@ int main(int argc, char *argv[]) {
 
 	FILE *fi = fopen(argv[1], "r");
 	if (!fi) return 3;
-	FILE *fo = fopen((string(argv[1]) + ".cherry").c_str(), "w");
-	int i, len;
+	lnNo_t i, len;
 	char s[MAX_LEN], nm_snap[MAX_LEN];
 
 	lnNo_t iter = 0, lns = 0;
@@ -62,6 +81,7 @@ int main(int argc, char *argv[]) {
 	vector<dtSz_t> sz_f(lns);
 	bool rd_success;
 
+	printf("* Starts cherrypick-ing the input file ... ");
 	rd_success = fgets(s, MAX_LEN, fi);
 	do {
 		vector<cp_state> stk;
@@ -106,7 +126,41 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	} while (rd_success);
-	for (i = 0; i < iter; ++i)
-		printf("%llu\n", sz_f[i]);
+//	for (i = 0; i < iter; ++i) printf("%llu\n", sz_f[i]);
+	fclose(fi);
+	puts("Done");
+
+	// starts forging the new massif file
+	printf("* Starts forging the new massif file ... ");
+	FILE *fo = fopen((string(argv[1]) + ".cherry").c_str(), "w");
+	fi = fopen(argv[1], "r");
+	iter = 0;
+	rd_success = fgets(s, MAX_LEN, fi);
+	do {
+		while (rd_success && s[0] != '#') {
+			fprintf(fo, s);
+			rd_success = fgets(s, MAX_LEN, fi);
+		}
+		if (!rd_success) break;
+		fprintf(fo, s);
+
+		fgets(nm_snap, MAX_LEN, fi);
+		fprintf(fo, nm_snap);
+		
+		while (fgets(s, MAX_LEN, fi) && s[0] == '#')
+			fprintf(fo, s);
+		fprintf(fo, s);
+		while (fgets(s, MAX_LEN, fi) && strstr(s, "=") != NULL)
+			fprintf(fo, s);
+		fprintf(fo, s);
+		
+		do {
+//			printf("%llu %s\n", sz_f[iter], s);
+			printSz(fo, s, sz_f[iter]);
+			++iter;
+		} while ( (rd_success = fgets(s, MAX_LEN, fi)) && s[0] != '#');
+	} while (rd_success);
+	puts("Done");
+
 	return 0;
 }
