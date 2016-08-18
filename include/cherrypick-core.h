92a8f311c1ca61356ff3ec2a4af00635715f1a35
Jasmine Chen <j.lin013@gmail.com>
2016-08-19 02:14:43 +0800

    Change formatting style to Google

--- include/cherrypick-core.h
+++ include/cherrypick-core.h
@@ -20,49 +20,41 @@ typedef unsigned long long lineNo_t;
 typedef unsigned long long dataSz_t;
 
 struct cp_state {
-    cp_state(lineNo_t _line, bool _match, bool _isLeaf = true)
-        : line(_line)
-        , match(_match)
-        , isLeaf(_isLeaf)
-    {
-    }
+  cp_state(lineNo_t _line, bool _match, bool _isLeaf = true)
+      : line(_line), match(_match), isLeaf(_isLeaf) {}
 
-    bool isLeaf;
-    lineNo_t line;
-    bool match;
+  bool isLeaf;
+  lineNo_t line;
+  bool match;
 };
 
 class cp_picker {
-public:
-    cp_picker()
-        : OPT_MERGE_STACKS(false)
-        , OPT_CLEAR_HEAP_EXTRA(false)
-    {
-    }
-    int parse_args(int argc, char* argv[]);
-    void initialize();
-    void cherrypick();
-    void forge();
+ public:
+  cp_picker() : OPT_MERGE_STACKS(false), OPT_CLEAR_HEAP_EXTRA(false) {}
+  int parse_args(int argc, char* argv[]);
+  void initialize();
+  void cherrypick();
+  void forge();
 
-    dataSz_t get_mem_peak();
+  dataSz_t get_mem_peak();
 
-private:
-    static const int MAX_LEN = 1002;
+ private:
+  static const int MAX_LEN = 1002;
 
-    FILE *fi, *fo;
+  FILE *fi, *fo;
 
-    char* filename;
-    char* pattern;
-    bool OPT_MERGE_STACKS;
-    bool OPT_CLEAR_HEAP_EXTRA;
+  char* filename;
+  char* pattern;
+  bool OPT_MERGE_STACKS;
+  bool OPT_CLEAR_HEAP_EXTRA;
 
-    lineNo_t lns;
+  lineNo_t lns;
 
-    vector<dataSz_t> sz_o;
-    vector<dataSz_t> sz_f;
-    vector<dataSz_t> mem_heap;
-    vector<dataSz_t> mem_stacks;
-    dataSz_t mem_peak;
+  vector<dataSz_t> sz_o;
+  vector<dataSz_t> sz_f;
+  vector<dataSz_t> mem_heap;
+  vector<dataSz_t> mem_stacks;
+  dataSz_t mem_peak;
 };
 
 inline dataSz_t getSz(const char* s);
